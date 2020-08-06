8a8ebfdac2fe18bd543ddab167218ad07e7d3cde
1andyn <andy.work.dev@gmail.com>
2020-08-05 19:48:08 -1000

    Updated baseline demo page

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -2,9 +2,8 @@
   <div class="text-center hero">
     <img class="mb-3 app-logo" src="/logo.png" alt="Vue.js logo" width="120" />
     <h1 class="mb-4">Days Since</h1>
-    <p class="lead">
-      Webapp to track days since a particular date. <i>Powered by Vue</i>
-    </p>
+    <p>Webapp to track days since a particular date. <i>Powered by Vue</i></p>
+    <img class="ctr-img" src ="/sample.jpg"/>
   </div>
 </template>
 
