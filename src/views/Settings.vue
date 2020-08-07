164543f88df8e5666dcc093c849609d14714d61a
1andyn <andy.work.dev@gmail.com>
2020-08-06 23:49:47 -1000

    New settings file

---
+++ src/views/Settings.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Settings />
+    <hr />
+  </div>
+</template>
+
+<script>
+import Settings from "../components/Settings";
+
+export default {
+  name: "settings",
+  components: {
+    Settings
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.next-steps {
+  .fa-link {
+    margin-right: 5px;
+  }
+}
+</style>
